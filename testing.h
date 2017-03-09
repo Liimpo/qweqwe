2b355280f0eb97469ce5ce1d22ebe5a99917031e
Liimpo <limpan.elwing@gmail.com>
2017-03-09 17:55:04 +0100

    missade

--- testing.h
+++ testing.h
@@ -2,13 +2,13 @@
 #define TESTING_H
 
 #include <QMessageBox>
-#include "minecounter.h"
 #include <QDialog>
 #include <QStatusBar>
 #include <QKeyEvent>
 #include <QPushButton>
 #include <QSignalMapper>
 #include "QtMath"
+#include <QTimer>
 #include "sweepboard.h"
 #include "b.h"
 
@@ -21,7 +21,7 @@ class testing : public QDialog
     Q_OBJECT
 
 public:
-    explicit testing(QWidget *parent = 0, int mineNr = 0, int startSize = 0);
+    explicit testing(QWidget *parent = 0, int gameMode = 10);
     void setNrOfMines(int mines);
     void setSizeOfBoard(int size);
     int getNrOfMines()const;
@@ -33,6 +33,7 @@ public:
     void winCase();
     void loseCase();
     void revealClose(int row, int column);
+    bool getIsWin()const;
 public slots:
     void refreshTime();
     void buttonIsPressed(QString);
@@ -45,12 +46,15 @@ private:
     Ui::testing *ui;
     int timeSinceStart;
     int mineVal;
+    int sizeOfBoard;
+    bool isAWin;
     int mineIsFlagged;
     int nrOfFlags;
-    int sizeOfBoard;
+    int gameMode;
     int nrOfMines, mineGoal;
     QMessageBox *box;
     b *ptr;
+    QTimer *timer;
     QSignalMapper *signalMapper, *signalMapper2;
     SweepBoard *game;
     int boardChecker[8][8];
